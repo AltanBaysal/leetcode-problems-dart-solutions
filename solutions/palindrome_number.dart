c4b633b6469c3bf8184e10842c2d97e2541ce5f7
AltanBaysal <altanbaysaldeveloper@gmail.com>
2022-07-06 20:52:28 +0300

    Palindrome Number

---
+++ solutions/palindrome_number.dart
@@ -0,0 +1,19 @@
+
+
+// https://leetcode.com/problems/palindrome-number/
+
+void main(List<String> args) {
+  print(isPalindrome(12121));
+}
+
+bool isPalindrome(int number) {
+  int reverseNumber = 0;
+
+  while(number > reverseNumber){
+    reverseNumber = reverseNumber*10 + number%10;
+    number = (number/10).floor();  
+  }
+
+  return (number == reverseNumber || number == (reverseNumber/10).floor());
+}
+
