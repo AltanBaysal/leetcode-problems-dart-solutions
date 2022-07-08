6b14fd20eb6ea63deee8ae0c713707e8cd41ecab
AltanBaysal <altanbaysaldeveloper@gmail.com>
2022-07-08 14:28:25 +0300

    Maximum Depth of Binary Tree Solved

---
+++ solutions/maximum_depth_of_binary_tree.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+class TreeNode {
+  int? val;
+  TreeNode? left;
+  TreeNode? right;
+
+  TreeNode({this.val, this.left, this.right});
+}
+
+final TreeNode mockTreeNode = TreeNode(
+  val: 5,
+  left: TreeNode(
+    val: 3,
+    left: TreeNode(val: 7),
+  ),
+  right: TreeNode(),
+);
+
+
+
+void main(List<String> args) {
+  print(maxDepth(mockTreeNode));
+}
+
+int maxDepth(TreeNode? treeNode) {
+  if (treeNode == null) return 0;
+  return max(maxDepth(treeNode.left), maxDepth(treeNode.right)) + 1;
+}
