42922880f93c4991c5a635d3c90e11a4ffd75836
AltanBaysal <altanbaysaldeveloper@gmail.com>
2022-07-14 14:04:58 +0300

    Create longest_substring_without_repeating_characters.dart

---
+++ solutions/medium/longest_substring_without_repeating_characters.dart
@@ -0,0 +1,23 @@
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+void main(List<String> args) {
+  print(lengthOfLongestSubstring("element"));
+}
+
+int lengthOfLongestSubstring(String s) {
+  String usedCharacters = "";
+  int length = 0;
+
+  for (int i = 0; i < s.length; i++) {
+    if (usedCharacters.indexOf(s[i]) > -1) {
+      usedCharacters = usedCharacters.split(s[i]).last;
+    } else {
+      usedCharacters = usedCharacters + s[i];
+    }
+    if (usedCharacters.length > length) {
+      length = usedCharacters.length;
+    }
+  }
+  return length;
+}
