3f10cd2d9d6b5a8642e4b9213494c2a485ed43cc
AltanBaysal <altanbaysaldeveloper@gmail.com>
2022-07-14 14:07:09 +0300

    Update longest_substring_without_repeating_characters.dart

--- solutions/medium/longest_substring_without_repeating_characters.dart
+++ solutions/medium/longest_substring_without_repeating_characters.dart
@@ -21,3 +21,20 @@ int lengthOfLongestSubstring(String s) {
   }
   return length;
 }
+
+int lengthOfLongestSubstringV1(String s) {
+  String usedCharacters = "";
+  int length = 0;
+
+  for (int i = 0; i < s.length; i++) {
+    if (usedCharacters.indexOf(s[i]) > -1) {
+      usedCharacters = usedCharacters.substring(usedCharacters.indexOf(s[i]));
+    } else {
+      usedCharacters = usedCharacters + s[i];
+    }
+    if (usedCharacters.length > length) {
+      length = usedCharacters.length;
+    }
+  }
+  return length;
+}
